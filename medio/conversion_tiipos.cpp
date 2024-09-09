cc5656b2f88c5ef26a96810cb45f5a34573d762f
NopAngel <angelgabrielnieto@outlook.com>
2024-09-09 17:53:41 -0400

    ..

---
+++ medio/conversion_tiipos.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+	double x = (int)3.14;
+
+	std::cout << x;
+
+	return 0;
+}
---
+++ medio/op_artimeticos.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+	int estudiantes = 20;
+
+	//estudiantes = estudiantes + 1;
+	//estudiantes += 1;
+	//estudiantes++;
+
+	// RESTA.
+
+	// estudiantes = estudiantes - 1;
+	//estudiantes -= 1;
+	//estudiantes--; - operador decremento.
+
+
+	// Multiplicación.
+
+	// estudiantes = estudiantes * 2;
+	// estudiantes *= 1;
+	
+	// Division
+
+	// estudiantes = estudiantes / 2
+	//estudiantes /= 1;
+
+	std::cout << estudiantes << std::endl;
+
+	return 0;
+}
