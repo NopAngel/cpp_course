127d3d5429d9625d9803edbc52550cf4118a2ec2
NopAngel <angelgabrielnieto@outlook.com>
2024-09-09 18:11:11 -0400

    ...

---
+++ medio/funciones_utiles.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+	double x = 3;
+	double y = 4;
+	double z;
+
+	//z = std::max(x, y); - ve las variables y la variable mayor la pone como valor.
+
+	// z = std::min(x, y); - ve las variables y devuelve el valor menor
+
+	//z = std::pow(2,3); - eleva 2 al 3.
+	//z = std::sqrt(2,3); - calcular la raiz cuadrada.
+	//z = std::abs(-150); - devuelve el valor absoluto de un numero.
+
+	std::cout << z;
+
+	return 0;
+}
