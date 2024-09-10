b5c79c5e218bddd49b1c32ea993fef954d5f093f
NopAngel <angelgabrielnieto@outlook.com>
2024-09-10 10:14:43 -0400

    .....

---
+++ medio/switches.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+	int mes;
+	std::cout << "Ingresa el Mes (1-12): ";
+	std::cin >> mes;
+	/*
+	* 
+	* MAL EJEMPLO
+	* 
+	* 
+	* LOS SWITCH SON UNA REPRESENTACION DE COMO NO DEBERIA SER UN IF, SI TIENES UNA CADENA MUY LARGA DE IFS,
+	* ES MEJOR USAR SWTICHES.
+	* 
+	* 
+	if (mes == 1) {
+		std::cout << "Es Enero";
+	}
+	else if (mes == 2) {
+		std::cout << "Es Febrero";
+	}
+	else if (mes == 3) {
+		std::cout << "Es Marzo";
+	}
+	else if (mes == 4) {
+		std::cout << "Es Abril";
+	}
+	else if (mes == 5) {
+		std::cout << "Es Mayo";
+	}
+	else if (mes == 6) {
+		std::cout << "Es Junio";
+	}
+	else if (mes == 7) {
+		std::cout << "Es Julio";
+	}
+	else if (mes == 8) {
+		std::cout << "Es Agosto";
+	}
+
+	*/
+
+}
